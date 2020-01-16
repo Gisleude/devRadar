cde654c55840d41b98bf7d561385a11ecc72c5bc
Gisleude Linhares <gisleudenem@live.com>
2020-01-16 15:56:01 -0300

    Building the web interface

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://root:gis995511@cluster0-xos1q.mongodb.net/omnist
   useUnifiedTopology: true,
 })
 
+app.use(cors(/*{ origin: 'http://localhost:3000'}*/))
 app.use(express.json());
 app.use(routes);
 
